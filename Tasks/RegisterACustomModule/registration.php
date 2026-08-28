d43ab04b19349ecff377a33aa7e836641bdcb92d
midnightseadance <shifratonya915@gmail.com>
2026-08-28 11:28:30 +0530

    Add RegisterACustomModule module
    Push Namespace and Module folders with registration and module definition file.

---
+++ Tasks/RegisterACustomModule/registration.php
@@ -0,0 +1,10 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Tasks_RegisterACustomModule',
+    __DIR__
+);
+
