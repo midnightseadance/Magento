035baa9439ba13a183d85b7186a4cdc93fbf1a26
midnightseadance <shifratonya915@gmail.com>
2026-09-03 12:30:56 +0530

    Refactor Index class constructor and method signatures

--- Tasks/CreateANewPage/Controller/Index/Index.php
+++ Tasks/CreateANewPage/Controller/Index/Index.php
@@ -2,21 +2,24 @@
 
 namespace Tasks\CreateANewPage\Controller\Index;
 
-use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
 
-class Index
+class Index extends Action
 {
-    protected $pageFactory;
+    protected PageFactory $pageFactory;
 
-    public function __construct(PageFactory $pageFactory)
-    {
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
         $this->pageFactory = $pageFactory;
+        parent::__construct($context);
     }
 
-    public function execute(): ResultInterface
+    public function execute()
     {
         return $this->pageFactory->create();
     }
 }
-
